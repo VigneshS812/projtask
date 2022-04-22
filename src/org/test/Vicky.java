04a6d598b426729159327fd308551026fb4d93c7
VigneshS812 <vigneshsv58@gmail.com>
2022-04-22 10:09:13 +0530

    logan

--- src/org/test/Vicky.java
+++ src/org/test/Vicky.java
@@ -3,7 +3,8 @@ package org.test;
 public class Vicky {
 private void vignesh() {
 System.out.println("test1 ");
-System.out.println("test1 ");
-
+System.out.println("test2 ");
+System.out.println("test3 ");
+System.out.println("test4 ");
 }
 }
